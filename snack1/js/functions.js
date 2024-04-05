cc370568d3e816cc3541607b546e3a20a90683d0
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-05 11:45:17 +0200

    feat: snack 1

---
+++ snack1/js/functions.js
@@ -0,0 +1,16 @@
+// funzione che genera N array
+
+/**
+ * Description: la funzione genera tante array quante indicate dall'utente
+ * @param {number} nArray 
+ * @returns {array} 
+ */
+function isNumberArray (nrArray) {
+    let rdnArray = [];
+    for(let i = 0; i < N; i++) {
+        rdnArray.push(Math.floor(Math.random() * 100)+1);
+    }
+
+    return rdnArray;
+}
+
---
+++ snack1/js/script.js
@@ -0,0 +1,7 @@
+// chiediamo all'utente di inserire un numero con prompt
+
+const N = parseInt(prompt("inserisci un numero"));
+
+// richiamo la funzione che genera N array
+
+console.log(isNumberArray(N));
